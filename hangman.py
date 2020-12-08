b0536cacc24d7434a6dd699b4a3d36504e6650ee
Steven Tyrie <TYRIES1@student.eit.ac.nz>
2020-12-09 09:56:11 +1300

    changed readme.txt

---
+++ hangman.py
@@ -0,0 +1,18 @@
+import pygame
+import random
+pygame.init()
+winHeight = 480; winWidth = 700; GREEN = (0,255,0)
+win = pygame.display.set_mode((winWidth, winHeight))
+pygame.display.set_caption("Hangman - by Steven")
+
+#-----------------------------------------------------
+# Main program here
+inPlay = True
+while inPlay:
+# Use loop to draw window
+win.fill(GREEN)
+pygame.display.update()
+pygame.time.delay(100)
+
+#-----------------------------------------------------
+pygame.quit() # always quit oygame when done!
